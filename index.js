765d27f4e5c1ea93eb6b03df6eff9e9395e8175f
Aniketgiram <giram.a@northeastern.edu>
2024-03-22 16:29:34 -0400

    base code added

---
+++ index.js
@@ -0,0 +1,22 @@
+import { cloudEvent as _cloudEvent } from '@google-cloud/functions-framework';
+import dotenv from 'dotenv'
+dotenv.config()
+
+_cloudEvent('verifyUser', cloudEvent => {
+    const base64name = cloudEvent.data.message.data;
+
+    var mg = new Mailgun(process.env.MAILGUN_API_KEY);
+
+    const data = base64name
+        ? Buffer.from(base64name, 'base64').toString()
+        : 'World';
+
+    // const decodedData = JSON.parse(dataBuffer.toString('utf8'));
+
+    sendText('sender@aniketgiram.me',
+        [data['username']],
+        'Verify USer Email Address',
+        function (err) { err && console.log(err) });
+
+    console.log(`Hello, ${name}!`);
+});
