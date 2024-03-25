ace2ca0a9abb595b84064bfd84b597925b6a1301
Aniketgiram <giram.a@northeastern.edu>
2024-03-25 16:59:00 -0400

    req obj removed

--- index.js
+++ index.js
@@ -26,18 +26,17 @@ functions.cloudEvent('verifyUser', cloudEvent => {
         from: "aniketgiram.me",
         to: "aniketgiram1@gmail.com",
         subject: 'Hello from Mailgun',
-        html: 'Hello, This is not a plain-text email, I wanted to test some spicy Mailgun sauce in NodeJS! <a href="http://0.0.0.0:3030/validate?' + req.params.mail + '">Click here to add your email address to a mailing list</a>'
+        html: 'Hello, This is not a plain-text email, I wanted to test some spicy Mailgun sauce in NodeJS! <a href="http://0.0.0.0:3030/validate?">Click here to add your email address to a mailing list</a>'
     };
 
     mailgun.messages().send(mail_data, function (err, body) {
         // If there is an error, render the error page
         if (err) {
-            res.render('error', { error: err });
             console.log("Got an error:", err);
         } else {
             // Here "submitted.jade" is the view file for this landing page
             // We pass the variable "email" from the url parameter in an object rendered by Jade
-            res.render('submitted', { email: mail_data.to });
+            console.log('submitted', { email: mail_data.to });
             console.log(body);
         }
     });
