8af96f678a7469179b7feebb4a2e1388cfa39be7
Aniketgiram <giram.a@northeastern.edu>
2024-03-25 17:15:11 -0400

    mailgun error fixed

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 const functions = require('@google-cloud/functions-framework');
 const dotenv = require('dotenv')
-// const formData = require('form-data');
+const formData = require('form-data');
 const Mailgun = require('mailgun.js');
-// const mailgun = new Mailgun(formData);
+const mailgun = new Mailgun(formData);
 
 dotenv.config()
 
-functions.cloudEvent('verifyUser', async cloudEvent => {
+functions.cloudEvent('verifyUser', cloudEvent => {
     const base64name = cloudEvent.data.message.data;
 
     // const mg = mailgun.client({ username: 'key', key: `key-${process.env.MAILGUN_API_KEY}` });
 
-    // const mg = mailgun.client({ username: 'api', key: process.env.MAILGUN_API_KEY });
-    const mailgun = new Mailgun({ apiKey: process.env.MAILGUN_API_KEY, domain: "aniketgiram.me" });
-
+    const mg = mailgun.client({ username: 'api', key: process.env.MAILGUN_API_KEY });
+    
     const data = base64name
         ? Buffer.from(base64name, 'base64').toString()
         : 'World';
@@ -26,18 +25,11 @@ functions.cloudEvent('verifyUser', async cloudEvent => {
         from: "aniketgiram.me",
         to: "aniketgiram1@gmail.com",
         subject: 'Hello from Mailgun',
+        message: "this is message",
         html: 'Hello, This is not a plain-text email, I wanted to test some spicy Mailgun sauce in NodeJS! <a href="http://0.0.0.0:3030/validate?">Click here to add your email address to a mailing list</a>'
     };
 
-    await mailgun.messages.send(mail_data, function (err, body) {
-        // If there is an error, render the error page
-        if (err) {
-            console.log("Got an error:", err);
-        } else {
-            // Here "submitted.jade" is the view file for this landing page
-            // We pass the variable "email" from the url parameter in an object rendered by Jade
-            console.log('submitted', { email: mail_data.to });
-            console.log(body);
-        }
-    });
+    mg.messages.create('aniketgiram.me', mail_data)
+        .then(msg => console.log(msg))
+        .catch(err => console.error(err));
 });
