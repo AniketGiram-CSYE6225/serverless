046115edcd0fe0d337204c10f2181573efb5c29e
Aniketgiram <giram.a@northeastern.edu>
2024-03-25 16:08:08 -0400

    mailgun fixing

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const functions = require('@google-cloud/functions-framework');
 const dotenv = require('dotenv')
 const formData = require('form-data');
 const Mailgun = require('mailgun.js');
-const mailgun = new Mailgun(formData);
+// const mailgun = new Mailgun(formData);
 
 dotenv.config()
 
@@ -10,7 +10,7 @@ functions.cloudEvent('verifyUser', cloudEvent => {
     const base64name = cloudEvent.data.message.data;
 
     // const mg = mailgun.client({ username: 'giram.a@northeastern.edu', key: process.env.MAILGUN_API_KEY});
-    const mg = mailgun.client({ key: process.env.MAILGUN_API_KEY });
+    // const mg = mailgun.client({ key: process.env.MAILGUN_API_KEY });
 
     const data = base64name
         ? Buffer.from(base64name, 'base64').toString()
@@ -19,13 +19,13 @@ functions.cloudEvent('verifyUser', cloudEvent => {
     const decodedData = JSON.parse(data);
     console.log(`decoded_data`, decodedData, typeof decodedData)
     console.log(`Hello, ${data}!`);
-    mg.messages.create('aniketgiram.me', {
-        from: "aniketgiram.me",
-        to: ["aniketgiram1@gmail.com"],
-        subject: "Hello",
-        text: "Testing some Mailgun awesomness!",
-        html: "<h1>Testing some Mailgun awesomness!</h1>"
-    })
-        .then(msg => console.log(msg)) // logs response data
-        .catch(err => console.error(err));
+    // mg.messages.create('aniketgiram.me', {
+    //     from: "aniketgiram.me",
+    //     to: ["aniketgiram1@gmail.com"],
+    //     subject: "Hello",
+    //     text: "Testing some Mailgun awesomness!",
+    //     html: "<h1>Testing some Mailgun awesomness!</h1>"
+    // })
+    //     .then(msg => console.log(msg)) // logs response data
+    //     .catch(err => console.error(err));
 });
