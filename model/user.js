5b3af70af459faa95619d9a2fffb739c9f129555
Aniketgiram <giram.a@northeastern.edu>
2024-03-25 19:37:59 -0400

    added db configs and models

---
+++ model/user.js
@@ -0,0 +1,50 @@
+import { DataTypes, Model, Sequelize } from 'sequelize'
+import sequelize from '../database/index.js'
+
+class User extends Model { }
+
+const user = {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+        primaryKey: true
+    },
+    firstName: {
+        type: DataTypes.STRING
+    },
+    lastName: {
+        type: DataTypes.STRING
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true,
+        }
+    },
+    password: {
+        type: DataTypes.STRING,
+    },
+    account_verified: {
+        type: DataTypes.BOOLEAN,
+        default: false
+    },
+    account_created: {
+        type: DataTypes.DATE
+    },
+    account_updated: {
+        type: DataTypes.DATE
+    }
+}
+
+User.init(user, {
+    sequelize,
+    timestamps: true,
+    createdAt: 'account_created',
+    updatedAt: 'account_updated',
+    modelName: 'User'
+});
+
+module.exports = User;
