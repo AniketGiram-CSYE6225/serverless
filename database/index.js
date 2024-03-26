f39692ed3aa6346fe5a537f4c65638304e53d14e
Aniketgiram <giram.a@northeastern.edu>
2024-03-26 14:42:56 -0400

    converted code to ES5

--- database/index.js
+++ database/index.js
@@ -1,21 +1,21 @@
-import { Sequelize } from 'sequelize'
-import dotenv from 'dotenv'
-dotenv.config()
+const Sequelize = require('sequelize');
+const dotenv = require('dotenv');
+dotenv.config();
 
 let db_conn = null
 
 function createConnection() {
     try {
         return new Sequelize({
-            dialect: process.env.DIALECT,
+            dialect: process.env.DB_DIALECT,
             database: process.env.DB_NAME,
             username: process.env.DB_USERNAME,
             password: process.env.DB_PASSWORD,
-            host: process.env.HOST
-        })
+            host: process.env.DB_HOST
+        });
     } catch (error) {
-        return error
+        return error;
     }
 }
 
-export default db_conn = db_conn != null ? db_conn : createConnection();
+module.exports = db_conn = db_conn !== null ? db_conn : createConnection();
